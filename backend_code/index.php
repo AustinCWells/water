a1041af2ad9f3489e737b903b2a58cc4364f851e
SpencerKaiser <skaiser@me.com>
2015-01-17 22:20:50 -0500

    Adding new functionality to backend code and polishing. add, remove, and get items are all fully functional and tested

--- backend_code/index.php
+++ backend_code/index.php
@@ -2,59 +2,101 @@
 
 require_once dirname(__FILE__) . "/utilities.php";
 
-//$counter = 0;
-//while (isset($_POST[$counter]))
-//{
-//    echo $_POST[$counter];
-//    $counter += 1;
-//}
-
-$values = $_POST;
-$count = count($values);
-
-echo "NUM PARAMS: ".$count;
+$args = $_POST;
+$numArgs = count($args);
 
 
-if (POSTAttributesPresent('userToken')) {
-    $userToken = $_POST['userToken'];
+if ($numArgs == 1) {
+    if(POSTAttributesPresent('userToken'))
+    {
+        $userToken = $_POST['userToken'];
 
-    // ROUTE REQUEST TO getTrackedItems()
-    getTrackedItems($userToken);
+        // ROUTE REQUEST TO getTrackedItems()
+        getTrackedItems($userToken);
 
-    // getTrackedItemsDemo($userToken);
+        // getTrackedItemsDemo($userToken);
+    }
+    else if (POSTAttributesPresent('itemNum'))
+    {
+        removeTrackedItem($_POST['itemNum']);
+    }
 }
-else if (POSTAttributesPresent('userToken', 'category', 'description', 'notificationMethod', 'url')) {
+else if (POSTAttributesPresent('userToken', 'description', 'notificationMethod', 'url', 'recurrence')) {
     $userToken = $_POST['userToken'];
-    $category = $_POST['category'];
     $description = $_POST['description'];
     $notificationMethod = $_POST['notificationMethod'];
     $url = $_POST['url'];
+    $recurrence = $_POST['recurrence'];
 
     // ROUTE REQUEST TO addTrackedItem()
-    addTrackedItem($userToken, $category, $description, $notificationMethod, $url);
+    addTrackedItem($userToken, $description, $notificationMethod, $url, $recurrence);
 }
 else {
-    echo "\nNo Parameters\n\n";
+    $userItems = $items->find();
+    echo json_encode(iterator_to_array($userItems));
 }
 
 
-
-
+/**
+ * Function that accepts a user token and returns all active items being tracked
+ *
+ * @param $userToken - User token created by Google authentication
+ */
 function getTrackedItems($userToken) {
-//    tasks = db.users.$userToken.find();
+    global $items;
 
+    $userItems = $items->find(array('userToken' => $userToken));
+
+    echo "\nUser [".$userToken."]:\n";
+    echo json_encode(iterator_to_array($userItems));
 }
+
+
+/**
+ * Function that accepts a series of parameters and adds a new item to the DB
+ *
+ * @param $userToken - User token created by Google authentication
+ * @param $description - Description of what is being tracked
+ * @param $notificationMethod - Method in which the user will be notified (phone, email, notification, etc.)
+ * @param $url - URL linking to the post (or page) being tracked
+ * @param $recurrence - How often notifications will take place (once/recurring)
+ */
+function addTrackedItem($userToken, $description, $notificationMethod, $url, $recurrence) {
+    global $items;
+
+    $newItem = array('userToken' => $userToken, 'description' => $description,
+        'notificationMethod' => $notificationMethod, 'url' => $url, 'recurrence' => $recurrence);
+    $items->insert($newItem);
+
+    echo "\nItem Added: \n";
+    echo json_encode($newItem);
+}
+
+
+/**
+ * Function to find an active item and remove it from the list of tracked items
+ *
+ * @param $itemNum - Unique identifier (primary key) for an individual item
+ */
+function removeTrackedItem($itemNum) {
+    global $items;
+
+    $items->remove(array('_id' => new MongoId($itemNum)));
+
+    echo "\nItem Removed";
+}
+
+
+
+
+
+
 
 function getTrackedItemsDemo($userToken) {
-    $task = array('itemNumber' => 123, 'category' => "Facebook", 'description' => "trackLikesForMyPost", 'contactMethod' => "phone", 'url' => "http://www.facebook.com");
-    $task2 = array('itemNumber' => 456, 'category' => "Twitter", 'description' => "trackRetweetsForMyTwat", 'contactMethod' => "all the things", 'url' => "http://www.jankopotomous.com");
-    $task3 = array('itemNumber' => 789, 'category' => "LinkedIn", 'description' => "trackPageViews", 'contactMethod' => "email", 'url' => "http://www.linkedin.com");
-    $tasks = array($task, $task2, $task3);
+    $item = array('itemNumber' => 123, 'category' => "Facebook", 'description' => "trackLikesForMyPost", 'contactMethod' => "phone", 'url' => "http://www.facebook.com");
+    $item2 = array('itemNumber' => 456, 'category' => "Twitter", 'description' => "trackRetweetsForMyTwat", 'contactMethod' => "all the things", 'url' => "http://www.jankopotomous.com");
+    $item3 = array('itemNumber' => 789, 'category' => "LinkedIn", 'description' => "trackPageViews", 'contactMethod' => "email", 'url' => "http://www.linkedin.com");
+    $items = array($item, $item2, $item3);
 
-    echo json_encode($tasks);
-}
-
-
-function addTrackedItem($userToken, $category, $description, $notificationMethod, $url) {
-
+    echo json_encode($items);
 }
--- backend_code/utilities.php
+++ backend_code/utilities.php
@@ -1,48 +1,49 @@
 <?php
 
-connectDB();
-
-
-/**
- * Function to connect to a MongoDB database.
- */
-function connectDB() {
-    try
-    {
-        $m = new MongoClient(); // connect
-        $db = $m->selectDB("test");
-    }
-    catch ( MongoConnectionException $e )
-    {
-        echo "\nCouldn't connect to mongodb, is the 'mongo' process running?\n\n";
-        exit();
-    }
+// CONNECT TO DB
+try
+{
+    $m = new MongoClient(); // connect
+    $db = $m->selectDB("water");
 }
+catch ( MongoConnectionException $e )
+{
+    echo "\nCouldn't connect to mongodb, is the 'mongo' process running?\n\n";
+    exit();
+}
+
+// CREATE COLLECTION VARIABLES
+$items = new MongoCollection($db, 'items');     // Items Collection
+$users = new MongoCollection($db, 'users');     // Users Collection
+
+
 
 /**
  * Function to determine whether all the arguments of the source
  * are identical to the test arguments.
+ *
  * @param $source - Source of original arguments
  * @param $args - Test arguments to compare with source
  * @return bool - Value indicating whether the args are identical
  */
 function attributesPresent($source, $args) {         // Accept source with args and the test arguments to compare against
-$argc = count($args);                                   // Get count of arguments
-for ($i = 0; $i < $argc; $i++) {                        // One at a time, compare the actual arg against test arg
-if (!isset($source[$args[$i]])) {
-return false;
-}
-}
-return true;                                            // If all args are identical (order sensitive), return TRUE
+    $argc = count($args);                                   // Get count of arguments
+    for ($i = 0; $i < $argc; $i++) {                        // One at a time, compare the actual arg against test arg
+        if (!isset($source[$args[$i]])) {
+            return false;
+        }
+    }
+    return true;                                            // If all args are identical (order sensitive), return TRUE
 }
 
 /**
  * Function to determine whether POST arguments exist.
+ *
  * @return bool - Represents whether arguments passed to it are
  * identical to those included within the first parameter
  */
 function POSTAttributesPresent() {
 // func_get_args is an array of the args passed to THIS function
-return attributesPresent($_POST, func_get_args());      // Check args in POST against test args passed to THIS function
+    return attributesPresent($_POST, func_get_args());      // Check args in POST against test args passed to THIS function
 }
 
