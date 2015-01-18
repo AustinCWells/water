5e4c39816d69d3104d0c530104cd5ad867c42a51
Austin Wells <acwells@smu.edu>
2015-01-17 22:17:05 -0500

    adding more junk

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -3,6 +3,25 @@ var lastElement = null;
 var lastElementOutline = null;
 var lastElementBackground = null; 
 var lastPotentialElement = null; 
+console.log("send message");
+chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+  console.log("checking response");
+});
+
+function timeout() {
+    setTimeout(function () {
+        var item = $("#MagicJankMHacksID");
+        var first = item.prop('outerHTML');
+        var second = lastElement.prop('outerHTML');
+        console.log("comparing " + first + " ___and__" + second);
+        if(lastElement == item)
+        	alert("same");
+        else
+        	alert("different");
+        lastElement = item; 
+        timeout();
+    }, 5000);
+}
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
@@ -24,6 +43,12 @@ chrome.runtime.onMessage.addListener(
 			mouse.x = e.clientX || e.pageX; 
 		    mouse.y = e.clientY || e.pageY;
 		    var itemOfInterest = document.elementFromPoint(mouse.x, mouse.y);
+
+		    // add a unique ID 
+		    itemOfInterest.id = "MagicJankMHacksID";
+
+		    
+
 		    if(lastElement){
 		    	lastElement.style.outline = "none";
 		    	lastElement.style.background = lastElementBackground;
@@ -33,7 +58,10 @@ chrome.runtime.onMessage.addListener(
 		    itemOfInterest.style.outline = "solid #5EC9B9 4px";
 		    lastElementBackground = itemOfInterest.style.background; 
 		    itemOfInterest.style.background = "#5EC9B9";
-
+		    // every five second pull out item of interest
+		    lastElement = $("#MagicJankMHacksID");
+		    timeout();
+		    
 		});
 
 		$(document).mouseover(function(e){
--- chrome_extension/popup.js
+++ chrome_extension/popup.js
@@ -1,8 +1,11 @@
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
     console.log(response.farewell);
   });
 });
+
+
 
 // // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // // Use of this source code is governed by a BSD-style license that can be
