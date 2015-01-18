939a66dcf7b95104dd1e0223bdecf2638fb88ec1
AustinCWells <austincwells@gmail.com>
2015-01-18 09:42:58 -0500

    pushing stuff

--- chrome_app/scripts/app.js
+++ chrome_app/scripts/app.js
@@ -1,25 +1,61 @@
 (function(){
 var app = angular.module('myapp', []); 
 var token = "MHacksMagicJankUserToken";
-app.controller('NotificationsController', ['$http', function($http){
-	this.notification = notification; 
-	
-	$http.get('https://104.236.120.63/water/backend_code?userToken=MHacksMagicJankUserToken').
-  	success(function(data, status, headers, config) {
-    	console.log(data);
-	  }).
+app.controller('NotificationsController', ['$http', '$scope', function($http, $scope){
+
+	// get items in list
+	function updateList() {
+    setTimeout(function () {
+        	$http.get('https://104.236.120.63/water/backend_code?userToken=MHacksMagicJankUserToken').
+		  		success(function(data, status, headers, config) {
+		  		console.log(data);
+		    	notificationCenter.notification = data;
+		    	for (var key in data) {
+		    		console.log("alert?: " + data[key].alert);
+		    		if(data[key].alert) {
+		    			var notification = new Notification("something has changed!\n" + data[key].description);
+		    			$http.get('https://104.236.120.63/water/backend_code?itemNum=' + key + '&success=true').
+		  				success(function(data, status, headers, config) {
+		  					console.log("notifiesUser");
+		  				});
+		    		}
+
+		    	}
+	 		 }).
+			  error(function(data, status, headers, config) {
+			  	console.log("error")
+			    // called asynchronously if an error occurs
+			    // or server returns response with an error status.
+			  });
+			  updateList();
+        }, 1000)
+	};
+
+    updateList();
+
+    function checkNotification() {
+    setTimeout(function () {
+        
+        }, 1000)
+	};
+
+	var notificationCenter = this; 
+	$scope.job;
+	$scope.myFunction = function(key){
+		delete notificationCenter.notification[key];
+		$scope.shouldDisplay = "hideIt";
+		console.log("key: " + key);
+		$http.get('https://104.236.120.63/water/backend_code?itemNum=' + key).
+  		success(function(data, status, headers, config) { }).
 	  error(function(data, status, headers, config) {
 	  	console.log("error")
-	    // called asynchronously if an error occurs
-	    // or server returns response with an error status.
 	  });
+	};
+
+
+	
 }]);
 
-var notification = {
-	a: "abc",
-	b: "bcd",
-	c: "dsfe"
-}
 
 })();
 
